8dfdbec828de5bba0ce5207f2161925f9c991b2a
Najeeb Vayal Valappil <najeeb.vayal-valappil.ext@flender-group.com>
2025-04-17 12:24:34 +0200

    test vars

--- main.tf
+++ main.tf
@@ -44,10 +44,11 @@ resource "azapi_resource" "this_environment" {
   location  = var.location
   name      = var.name
   dynamic "identity" {
-    for_each = var.identity == null ? [] : [var.identity]
+    for_each = local.managed_identities.system_assigned_user_assigned
+
     content {
-      type = identity.value.type
-      identity_ids = try(identity.value.identity_ids, [])
+      type         = identity.value.type
+      identity_ids = identity.value.user_assigned_resource_ids
     }
   }
   parent_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${var.resource_group_name}"
