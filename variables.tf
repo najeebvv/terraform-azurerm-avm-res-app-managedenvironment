8908192a5b4227a3b3966d9ed3a66391ca50197e
Najeeb Vayal Valappil <najeeb.vayal-valappil.ext@flender-group.com>
2025-04-17 12:27:36 +0200

    test vars

--- variables.tf
+++ variables.tf
@@ -35,9 +35,19 @@ variable "dapr_application_insights_connection_string" {
   sensitive   = true
 }
 
-variable "identity" {
+variable "managed_identities" {
+  type = object({
+    system_assigned            = optional(bool, false)
+    user_assigned_resource_ids = optional(set(string), [])
+  })
   default     = {}
-  description = "Identity."
+  description = <<DESCRIPTION
+  Controls the Managed Identity configuration on this resource. The following properties can be specified:
+
+  - `system_assigned` - (Optional) Specifies if the System Assigned Managed Identity should be enabled.
+  - `user_assigned_resource_ids` - (Optional) Specifies a list of User Assigned Managed Identity resource IDs to be assigned to this resource.
+  DESCRIPTION
+  nullable    = false
 }
 
 variable "diagnostic_settings" {
